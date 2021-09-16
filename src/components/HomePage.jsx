f338036230700c8c641a05373668cb902030818d
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-09-16 21:18:29 +0500

    udpates

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,29 @@ import "../css/HomePage.css";
 import { NavLink } from "react-router-dom";
 const HomePage = () => {
   return (
-    <div>
-      HomePage <NavLink to="/product">Start your Order</NavLink>{" "}
+    <div className="homePage">
+      <div className="product-image">
+        <img
+          src="https://luckyleather.com.pk/wp-content/uploads/2018/09/Johnson_Mens_Leather_Jacket__25775_zoom.jpg"
+          alt="product"
+        />
+      </div>
+      <div className="product-content">
+        <div className="product-details">
+          Title
+          <br />
+          Description
+          <br />
+          Stock
+          <br />
+          Brand
+          <br />
+          Price
+        </div>
+        <NavLink to="/product" className="startOrder">
+          Start your Order
+        </NavLink>
+      </div>
     </div>
   );
 };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,42 @@
 import "../css/Navigation.css";
 
 const Navigation = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navigation">
+      <nav className="navbar navbar-expand-md">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#contact">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  About Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
