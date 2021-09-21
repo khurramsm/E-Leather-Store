4d08190e872475a213cdd1207a0c4874a5dc3b46
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-09-21 13:57:51 +0500

    updates

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -12,46 +12,46 @@ const ProductPage = () => {
   const [productCart, setProductCart] = useState(0);
 
   return (
-    <div>
-      <h2>ProductPage</h2>
-      <select name="item" id="item">
-        <option value="leather-jacket">leather-jacket</option>
-        <option value="leather-lakeland">leather-lakeland</option>
-      </select>
-      <br />
-      <h5>Step 1</h5>
-      upload your design
-      <h5>Step 2</h5>
-      {[
-        "Select Leather Color",
-        "Select Leather Type",
-        "Select Lining Color",
-        "Select Hardware Color",
-        "Select Studs ",
-      ].map((item, ind) => (
-        <button
-          key={ind}
-          className="customizeProductButton"
-          onClick={() => setLeatherColor(!leatherColor)}
-        >
-          {item}
-          <span>
-            {leatherColor ? (
-              <i className="fa fa-chevron-down"></i>
-            ) : (
-              <i className="fa fa-chevron-up"></i>
-            )}
-          </span>
-        </button>
-      ))}
-      <h5>Step 3</h5>
-      Select your Size
-      <br />
-      <button onClick={() => setProductCart(productCart + 1)}>
-        Add to Cart
-      </button>
-      <br />
-      {productCart > 0 && <NavLink to="/cart">Proceed to Cart</NavLink>}
+    <div className="product-page container-fluid bg-primary">
+      <div className="container p-3 bg-secondary">
+        <div className="row bg-warning p-3">
+          <div className="col-md-12">
+            <h5>Step 1</h5>
+            upload your design
+            <h5>Step 2</h5>
+            {[
+              "Select Leather Color",
+              "Select Leather Type",
+              "Select Lining Color",
+              "Select Hardware Color",
+              "Select Studs ",
+            ].map((item, ind) => (
+              <button
+                key={ind}
+                className="customizeProductButton"
+                onClick={() => setLeatherColor(!leatherColor)}
+              >
+                {item}
+                <span>
+                  {leatherColor ? (
+                    <i className="fa fa-chevron-down"></i>
+                  ) : (
+                    <i className="fa fa-chevron-up"></i>
+                  )}
+                </span>
+              </button>
+            ))}
+            <h5>Step 3</h5>
+            Select your Size
+            <br />
+            <button onClick={() => setProductCart(productCart + 1)}>
+              Add to Cart
+            </button>
+            <br />
+            {productCart > 0 && <NavLink to="/cart">Proceed to Cart</NavLink>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
